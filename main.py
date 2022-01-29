45259d5a7f65634df3d14f128e3c7719024b25d5
edtallison <edtallison@gmail.com>
2022-01-29 20:02:08 +1300

    Begin to fix positioning of squares

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import sys, pygame
+import math
 pygame.init()
 
 size = width, height = 1000, 300
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Activity Tracker')
 
-days = [5,1,0,10,8,7,6,4,3,8,9,0,5,8,10,15,7]*3
+days = [5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]*9
 
 for i in range(len(days)):
+    x_pos = (10 + i*25) % 950
+    y_pos = 25 + math.ceil(((i - 49) / 40)) * 25
     colour = (0, 255*days[i]/max(days), 0)
     square = pygame.Surface((20,20))
     square.fill(colour)
-    screen.blit(square, (10+25*i, 10))
+    screen.blit(square, (x_pos, y_pos))
 
 pygame.display.flip()
 
