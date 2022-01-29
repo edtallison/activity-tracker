53bcca447d11102f9ffe80a3c27e33fbdf548086
edtallison <edtallison@gmail.com>
2022-01-29 19:43:04 +1300

    Create initial loop for generating squares

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ size = width, height = 1000, 300
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Activity Tracker')
 
+green = 0, 255, 0
+
+for i in range(30):
+    square = pygame.Surface((20,20))
+    square.fill(green)
+    screen.blit(square, (10+25*i, 10))
+
+pygame.display.flip()
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
