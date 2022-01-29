1ede4db6b8777a0b066d0c3fd529546a435c0dc6
edtallison <edtallison@gmail.com>
2022-01-29 19:47:55 +1300

    Add variation in colour

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ size = width, height = 1000, 300
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Activity Tracker')
 
-green = 0, 255, 0
+days = [5,1,0,10,8,7,6,4,3,8,9,0,5]
 
-for i in range(30):
+# green = 0, 255, 0
+
+for i in range(len(days)):
+    colour = (0, 255*days[i]/max(days), 0)
     square = pygame.Surface((20,20))
-    square.fill(green)
+    square.fill(colour)
     screen.blit(square, (10+25*i, 10))
 
 pygame.display.flip()
