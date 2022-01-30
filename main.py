50ee4e178f9c3d49dd31690c3304bcd0c891e32e
edtallison <edtallison@gmail.com>
2022-01-30 14:11:43 +1300

    Increase size/capacity of UI

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import sys, pygame
 import math
 pygame.init()
 
-size = width, height = 995, 300
+size = width, height = 1020, 300
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Activity Tracker')
 
-days = [i*i for i in range(200)]
+days = [i*i for i in range(300)]
 
 for i in range(len(days)):
-    x_pos = (10 + i*25) % 975
-    y_pos = 10 + math.ceil(((i - 38) / 39)) * 25
-    colour = (0, 255*days[i]/max(days), 0)
+    x_pos = (10 + i*25) % 1000
+    y_pos = 10 + math.ceil(((i - 39) / 40)) * 25
+    
+    if i == 0:
+        colour = (20,20,20)
+    else:
+        colour = (0, max(255*days[i]/max(days),30), 0)
     square = pygame.Surface((20,20))
     square.fill(colour)
     screen.blit(square, (x_pos, y_pos))
